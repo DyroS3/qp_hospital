b7718b4b1e850e3e628da5be5a136fcb5f4fefca
Arlo Fonseca <thedondada7801@gmail.com>
2022-08-22 12:13:10 -0400

    prettier

--- client/main.lua
+++ client/main.lua
@@ -1,116 +1,120 @@
 ---@param state usedState
 ---@return boolean
 function setState(state, bool, replicated)
-   LocalPlayer.state:set(state, bool, replicated)
+	LocalPlayer.state:set(state, bool, replicated)
 end
 
 local function beginTreatment()
-   if
-    lib.progressCircle({
-      duration = 3500,
-      position = "bottom",
-      label = "Signing in..",
-      disable = { move = false },
-      anim = { dict = config.treatment.dict, clip = config.treatment.anim },
-    })
-   then
-      SetEntityHealth(cache.ped, 200)
-      lib.defaultNotify({
-         title = "Medical Center",
-         position = "top",
-         description = "You have successfully been treated and can go on your way.",
-         status = "success",
-      })
-   end
+	if
+		lib.progressCircle({
+			duration = 3500,
+			position = "bottom",
+			label = "Signing in..",
+			disable = { move = false },
+			anim = { dict = config.treatment.dict, clip = config.treatment.anim },
+		})
+	then
+		SetEntityHealth(cache.ped, 200)
+		lib.defaultNotify({
+			title = "Medical Center",
+			position = "top",
+			description = "You have successfully been treated and can go on your way.",
+			status = "success",
+		})
+	end
 end
 
 local function whileBleeding()
-   local ped = cache.ped
-   local health = GetEntityHealth(ped)
+	local ped = cache.ped
+	local health = GetEntityHealth(ped)
 
-   if health <= 150 then
-      setState('isBleeding', true, true)
-      SetEntityHealth(cache.ped, health - 1)
-      lib.defaultNotify({ title = "", position = "top", description = "You need medical attention, visit the Medical Center.", status = "error" })
+	if health <= 150 then
+		setState("isBleeding", true, true)
+		SetEntityHealth(cache.ped, health - 1)
+		lib.defaultNotify({
+			title = "",
+			position = "top",
+			description = "You need medical attention, visit the Medical Center.",
+			status = "error",
+		})
 
-      lib.requestAnimSet(config.bleeding.anim)
-      SetPedMovementClipset(ped, config.bleeding.anim, true)
-      ShakeGameplayCam(config.bleeding.effect, config.bleeding.intensity)
+		lib.requestAnimSet(config.bleeding.anim)
+		SetPedMovementClipset(ped, config.bleeding.anim, true)
+		ShakeGameplayCam(config.bleeding.effect, config.bleeding.intensity)
 
-      DoScreenFadeOut(500)
-      while not IsScreenFadedOut() do
-         Wait(100)
-      end
+		DoScreenFadeOut(500)
+		while not IsScreenFadedOut() do
+			Wait(100)
+		end
 
-      DoScreenFadeIn(500)
-   elseif health >= 175 then
-      setState('isBleeding', false, true)
-      ResetPedMovementClipset(ped, 0)
-      ResetPedStrafeClipset(ped, 0)
-      StopAllScreenEffects(cache.ped)
-   end
+		DoScreenFadeIn(500)
+	elseif health >= 175 then
+		setState("isBleeding", false, true)
+		ResetPedMovementClipset(ped, 0)
+		ResetPedStrafeClipset(ped, 0)
+		StopAllScreenEffects(cache.ped)
+	end
 end
 
 RegisterNetEvent("qp_hospital:requestTreatment", function()
-   if config.treatment.costmoney then
-      lib.callback("qp_hospital:startTreatment", false, function(response)
-         if response then
-            beginTreatment()
-         end
-      end)
-   elseif not config.treatment.costmoney then
-      beginTreatment()
-   end
+	if config.treatment.costmoney then
+		lib.callback("qp_hospital:startTreatment", false, function(response)
+			if response then
+				beginTreatment()
+			end
+		end)
+	elseif not config.treatment.costmoney then
+		beginTreatment()
+	end
 end)
 
 RegisterNetEvent("qp_hospital:requestBandage", function()
-   lib.callback("qp_hospital:purchaseBandage", false, function(success)
-      if success then
-         print("bandage purchased")
-      elseif not success then
-         print("your broke")
-      end
-   end)
+	lib.callback("qp_hospital:purchaseBandage", false, function(success)
+		if success then
+			print("bandage purchased")
+		elseif not success then
+			print("your broke")
+		end
+	end)
 end)
 
 SetInterval(function()
-   blip = AddBlipForCoord(config.hospital.x, config.hospital.y)
-   SetBlipSprite(blip, config.hospital.id)
-   SetBlipScale(blip, config.hospital.size)
-   SetBlipColour(blip, config.hospital.color)
-   SetBlipAsShortRange(blip, true)
-   BeginTextCommandSetBlipName("STRING")
-   AddTextComponentString(config.hospital.name)
-   EndTextCommandSetBlipName(blip)
+	blip = AddBlipForCoord(config.hospital.x, config.hospital.y)
+	SetBlipSprite(blip, config.hospital.id)
+	SetBlipScale(blip, config.hospital.size)
+	SetBlipColour(blip, config.hospital.color)
+	SetBlipAsShortRange(blip, true)
+	BeginTextCommandSetBlipName("STRING")
+	AddTextComponentString(config.hospital.name)
+	EndTextCommandSetBlipName(blip)
 end)
 
 SetInterval(function()
-   whileBleeding()
-   Wait(2600)
+	whileBleeding()
+	Wait(2600)
 end)
 
 if config.general.debug then
-   local function effectDebug()
-      StopAllScreenEffects(cache.ped)
-      ResetPedMovementClipset(cache.ped, 0)
-      print("debug: effects")
-   end
-   RegisterCommand("debug:effect", effectDebug)
+	local function effectDebug()
+		StopAllScreenEffects(cache.ped)
+		ResetPedMovementClipset(cache.ped, 0)
+		print("debug: effects")
+	end
+	RegisterCommand("debug:effect", effectDebug)
 end
 
 if config.general.debug then
-   local function healDebug()
-      SetEntityHealth(cache.ped, 200)
-      print("debug: heal")
-   end
-   RegisterCommand("debug:heal", healDebug)
+	local function healDebug()
+		SetEntityHealth(cache.ped, 200)
+		print("debug: heal")
+	end
+	RegisterCommand("debug:heal", healDebug)
 end
 
 if config.general.debug then
-   local function bleedingDebug()
-      SetEntityHealth(cache.ped, 130)
-      print("debug: bleeding")
-   end
-   RegisterCommand("debug:bleed", bleedingDebug)
+	local function bleedingDebug()
+		SetEntityHealth(cache.ped, 130)
+		print("debug: bleeding")
+	end
+	RegisterCommand("debug:bleed", bleedingDebug)
 end
-
--- server/main.lua
+++ server/main.lua
@@ -1,59 +1,88 @@
 local ox_inventory = exports.ox_inventory
 GlobalState.hospitalState = true
 
-
 ---@param source number
 ---@return boolean
 lib.callback.register("qp_hospital:startTreatment", function(source)
-    if not GlobalState.hospitalState then return end
+	if not GlobalState.hospitalState then return end
 
-    local entityId = source
-    local playerState = getState(entityId, 'isBleeding')
+	local entityId = source
+	local playerState = getState(entityId, "isBleeding")
 
-    if not playerState then
-        TriggerClientEvent("ox_lib:defaultNotify", source, { status = "error", title = 'Medical Center', position = "top", description = 'You do not need medical attention at the moment' })
-            print(entityId, playerState)
-        return
-    end
+	if not playerState then
+		TriggerClientEvent("ox_lib:defaultNotify", source, {
+				status = "error",
+				title = "Medical Center",
+				position = "top",
+				description = "You do not need medical attention at the moment",
+			}
+		)
+		print(entityId, playerState)
+		return
+	end
 
-    local src = source
-    local cache = nil
+	local src = source
+	local cache = nil
 
-    if ox_inventory:GetItem(src, config.treatment.item).count >= config.treatment.price then
-        TriggerClientEvent("ox_lib:defaultNotify", source, { status = "success", title = 'Medical Center', position = "top", description = "You were billed by the Medical Center." })
-        ox_inventory:RemoveItem(src, config.treatment.item, config.treatment.price)
-        cache = true
-    else
-        TriggerClientEvent("ox_lib:defaultNotify", source, { status = "error", title = 'Medical Center', position = "top", description = "You do not have enough cash, please visit the local bank." })
-        cache = false
-    end
+	if ox_inventory:GetItem(src, config.treatment.item).count >= config.treatment.price then
+		TriggerClientEvent("ox_lib:defaultNotify", source, {
+				status = "success",
+				title = "Medical Center",
+				position = "top",
+				description = "You were billed by the Medical Center.",
+			}
+		)
+		ox_inventory:RemoveItem(src, config.treatment.item, config.treatment.price)
+		cache = true
+	else
+		TriggerClientEvent("ox_lib:defaultNotify", source, {
+				status = "error",
+				title = "Medical Center",
+				position = "top",
+				description = "You do not have enough cash, please visit the local bank.",
+			}
+		)
+		cache = false
+	end
 
-    while cache == nil do
-        Wait(50)
-    end
+	while cache == nil do
+		Wait(50)
+	end
 
-    return cache
+	return cache
 end)
 
 lib.callback.register("qp_hospital:purchaseBandage", function(source)
-   if not GlobalState.hospitalState then return end
+	if not GlobalState.hospitalState then return end
 
-   local src = source
-   local cache = nil
+	local src = source
+	local cache = nil
 
-   if ox_inventory:GetItem(src, config.treatment.item).count >= 300 then
-      ox_inventory:RemoveItem(src, config.treatment.item, config.bandage.price)
-      TriggerClientEvent("ox_lib:defaultNotify", source, { status = "success", title = 'Medical Center', position = "top", description = "You have successfully purchased a bandage." })
-      ox_inventory:AddItem(src, config.bandage.item, 1)
-      cache = true
-   else
-      TriggerClientEvent("ox_lib:defaultNotify", source, { status = "error", title = 'Medical Center', position = "top", description = "You do not have enough cash, please visit the local bank." })
-      cache = false
-   end
+	if ox_inventory:GetItem(src, config.treatment.item).count >= 300 then
+		ox_inventory:RemoveItem(src, config.treatment.item, config.bandage.price)
+		TriggerClientEvent("ox_lib:defaultNotify", source, {
+				status = "success",
+				title = "Medical Center",
+				position = "top",
+				description = "You have successfully purchased a bandage.",
+			}
+		)
+		ox_inventory:AddItem(src, config.bandage.item, 1)
+		cache = true
+	else
+		TriggerClientEvent("ox_lib:defaultNotify", source, {
+				status = "error",
+				title = "Medical Center",
+				position = "top",
+				description = "You do not have enough cash, please visit the local bank.",
+			}
+		)
+		cache = false
+	end
 
-   while cache == nil do
-      Wait(50)
-   end
+	while cache == nil do
+		Wait(50)
+	end
 
-   return cache
+	return cache
 end)
